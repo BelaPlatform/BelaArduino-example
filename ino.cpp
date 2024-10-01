eaddf51e2eda5a979f4c77ad9d753164df2da907
Giulio Moro <giuliomoro@yahoo.it>
2024-10-01 00:16:42 +0000

    Initial commit

---
+++ ino.cpp
@@ -0,0 +1,67 @@
+#include <libraries/BelaArduino/Arduino.h>
+#include <libraries/BelaArduino/PdArduino.h>
+
+// This function is called when a message is sent from a Pd
+// patch. You can use it to communicate from Pd back to your
+// Arduino-style code.
+void pdReceiveMsg(const char* symbol, float* data, size_t length)
+{
+	Serial.print("Received ");
+	Serial.print(symbol);
+	Serial.print("[");
+	Serial.print(length);
+	Serial.print("]:");
+	for(size_t n = 0; n < length; ++n)
+	{
+		Serial.print(" ");
+		Serial.print(data[n]);
+	}
+	Serial.println();
+}
+
+void setup()
+{
+	Serial.println("Arduino setup\n");
+	// Set up the digital pins
+	pinMode(0, OUTPUT);
+	pinMode(1, OUTPUT);
+	pinMode(2, INPUT);
+	pinMode(3, OUTPUT);
+	pinMode(4, OUTPUT);
+	pinMode(5, OUTPUT);
+	pinMode(6, OUTPUT);
+
+	// Load sound
+	pdSendMessage("loadSound", 0, "kick.wav");
+	pdSendMessage("loadSound", 1, "snare.wav");
+}
+
+void loop()
+{
+	// Example of using a shift register:
+	static unsigned int count = 0;
+	shiftOut(4, 5, 6, LSBFIRST, 16, count);
+
+	// To use a custom GUI, you need to #define ENABLE_GUI in BelaArduino.cpp
+	float myArray[2];
+	myArray[1] = count & 1;
+	myArray[0] = count++;
+	gui.sendBuffer(0, myArray);
+
+	// Example of playing sounds (see _main.pd) and blinking LEDs
+	pdSendMessage("playSound", 0);
+	digitalWrite(0, HIGH);
+	digitalWrite(1, digitalRead(2));
+	pwmWrite(3, 0.05);
+	delay(100);
+	pdSendMessage("stopSound", 0);
+	delay(400);
+
+	pdSendMessage("playSound", 1);
+	digitalWrite(0, LOW);
+	digitalWrite(1, digitalRead(2));
+	pwmWrite(3, 0.3);
+	delay(100);
+	pdSendMessage("stopSound", 1);
+	delay(400);
+}
---
+++ render.cpp
@@ -0,0 +1,45 @@
+#include <libraries/BelaLibpd/BelaLibpd.h>
+#include <libraries/BelaArduino/BelaArduino.h>
+#include <libraries/Watcher/Watcher.h>
+
+Gui wmGui;
+WatcherManager wm(wmGui);
+Watcher<float> myvar("myvar"); // this is the user-define watcher
+
+bool setup(BelaContext* context, void* userData)
+{
+	BelaLibpdSettings settings;
+	settings.useIoThreaded = false;
+	settings.useGui = false; // we want to use the BelaArduino GUI, which is controlled here and in ino.cpp
+	settings.messageHook = BelaArduino_messageHook;
+	settings.listHook = BelaArduino_listHook;
+	settings.floatHook = BelaArduino_floatHook;
+	if(!BelaLibpd_setup(context, userData, settings))
+		return false;
+	BelaArduinoSettings arduinoSettings;
+	arduinoSettings.controlPanelAudio = false;
+	if(!BelaArduino_setup(context, userData, arduinoSettings))
+		return false;
+	unsigned int wsPort = 5557;
+	wm.getGui().setup("/libraries/Watcher/sketch-watcher-example.js", wsPort);
+	printf("sketch-watcher.js GUI at bela.local/gui?wsPort=%u\n", wsPort);
+	return true;
+}
+
+void render(BelaContext* context, void* userData)
+{
+	if(wm.getGui().numConnections())
+		wm.tick(context->audioFramesElapsed);
+	myvar = myvar + 0.001;
+	if(myvar >= 1)
+		myvar = 0;
+	BelaArduino_renderTop(context, userData);
+	BelaLibpd_render(context, userData);
+	BelaArduino_renderBottom(context, userData);
+}
+
+void cleanup(BelaContext* context, void* userData)
+{
+	BelaArduino_cleanup(context, userData);
+	BelaLibpd_cleanup(context, userData);
+}
